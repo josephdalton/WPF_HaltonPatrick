fed4f0a3d60e95699fc14759b87ba42d2cf5a053
patrickhalton04 <patrickhalton@Patricks-MacBook-Pro-14.local>
2013-11-13 15:03:22 -0500

    3 changes

--- Conditionals_Personal/Conditionals_Personal.js
+++ Conditionals_Personal/Conditionals_Personal.js
@@ -1,12 +1,20 @@
 /**
  * Created by patrickhalton on 11/13/13.
  */
+var now;
+var date = prompt('What is the date?');
+
+if (date=="11/13/13" || "11/15/13" || "11/18/13" ){
+
+    now = ('Reminder: You have class!')
+}
+else {
 
 
+   now = ('Have a Great Day')
+}
 
-
-
-
+console.log(now);
 
 
 
