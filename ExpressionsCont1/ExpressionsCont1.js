337efea36d4874cb58e79775ba8a3e417e1f350a
patrickhalton04 <patrickhalton@Patricks-MacBook-Pro-14.local>
2013-11-13 10:21:59 -0500

    Quiz1

---
+++ ExpressionsCont1/ExpressionsCont1.js
@@ -0,0 +1,44 @@
+/**
+ * Created by patrickhalton on 11/11/13.
+ */
+
+var items = prompt('How many items on the list?');
+
+
+var shoppingList = [];
+
+for (i=0; i<items; i=i+1){
+    shoppingList[i] = prompt('What is in on the shopping list?')
+
+}
+console.log(shoppingList);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ WPTEST/Quiz1_PatrickHalton.js
@@ -0,0 +1,118 @@
+///**
+// * Created by patrickhalton on 11/13/13.
+// */
+//
+//var colors = prompt('How many colors are on your shirt?')//first prompted value is asking the user how many colors areon their shirt
+//var MyArray = [];//Declaring the expression is an array
+//for (i=0; i<colors; i++);//instead of writing all the number in the array, this for loop puts as many colors in the array that the user chooses
+//
+//MyArray[i] = prompt('What colors?');//states that the array i created is prompted
+//console.log(MyArray);//logging out the array
+//
+//
+//
+//
+//
+//
+//
+//
+//var R;//declaring a variable for an answer to a question
+//var question = prompt('How many times do you brush your teeth a day?');//prompting the user a question
+//
+//if (question>=2){//if statement saying if the user put in 2 or more as the answer The 'R' varables states "very good"
+//
+//    R = 'very good!';
+//
+//}
+//
+//else R = 'Brush More'//else statement says if the user puts less than 2 it tells the user to "brush more"
+//
+//console.log(R);//logging out the variable 'R'
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//var i = prompt ('What is the average of the total home runs?')
+//var ans;
+//
+//;
+//if (i==17){
+//
+//    ans = "correct";
+//}
+//else {ans = "incorrect";}
+//
+//console.log(ans);
+//
+//var HR1 = (5);//declaring all the batters home runs in seperate variables
+//var HR2 = (9);
+//var HR3 = (12);
+//var HR4 = (3);
+//var HR5 = (23);
+//var HR6 = (16);
+////************ADDITION AND DIVISION*********************
+//var BattingAverage = (HR1 + HR2 + HR3 + HR4 + HR5 + HR6)/4//putting the addition in parentheses tell the computer to do the addition before divison
+
+
+//    console.log(BattingAverage);
+
+//**************MULTIPLICATION*****************
+//+ is used whe adding 2 vaiables or strings
+
+//var A = 'there';
+//var B = 'fore';
+//var final = (A + B);
+//console.log(final);
+
+//var www = 2;
+//var eee = 6;
+//var ans =(www*eee);
+//console.log(ans);
+
+//*************SUBTRACTION**********
+
+//var too = 56;
+//var foo = 31;
+//var ans = (too - foo);
+//
+//console.log(ans);
+
+
+//****************Modulus*********
+//var q = 23;
+//var w = 7;
+//
+//var ans = (q / w%2);//division with remainder of 2
+//console.log(ans);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
